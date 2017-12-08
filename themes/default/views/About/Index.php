4f0ad91d76c9cc92c93d0b03ffe75a1143835a1e
Ashley Blewer <ashley.blewer@gmail.com>
2017-12-08 18:23:09 -0500

    init commit

---
+++ themes/default/views/About/Index.php
@@ -0,0 +1,15 @@
+<?php
+	MetaTagManager::setWindowTitle($this->request->config->get("app_display_name").": About");
+?>
+
+	<div class="row">
+		<div class="col-sm-12">
+			<H1><?php print _t("About"); ?></H1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-8">
+			<h3>About The Archives</h3>
+			<p></p>
+		</div>
+	</div>
